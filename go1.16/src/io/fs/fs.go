5a4e4c4530e4eddd5c456b72c7e7a6f011ce4028
laijh <761774955@qq.com>
2021-04-07 00:38:51 +0800

    fs

--- go1.16/src/io/fs/fs.go
+++ go1.16/src/io/fs/fs.go
@@ -77,12 +77,29 @@ func errNotExist() error   { return oserror.ErrNotExist }
 func errClosed() error     { return oserror.ErrClosed }
 
 type FileInfo interface {
-	Name() string       // base name of the file
-	Size() int64        // length in bytes for regular files; system-dependent for others
-	Mode() FileMode     // file mode bits
-	ModTime() time.Time // modification time
-	IsDir() bool        // abbreviation for Mode().IsDir()
-	Sys() interface{}   // underlying data source (can return nil)
+	Name() string       // base name of the file，文件的名字
+	Size() int64        // length in bytes for regular files; system-dependent for others，普通文件返回值表示其大小；其他文件的返回值含义各系统不同
+	Mode() FileMode     // file mode bits，文件的模式位
+	ModTime() time.Time // modification time，文件的修改时间
+	IsDir() bool        // abbreviation for Mode().IsDir()，等价于Mode().IsDir()
+	/*
+	struct stat {
+	    dev_t    st_dev;    // 设备 ID
+	    ino_t    st_ino;    // 文件 i 节点号
+	    mode_t    st_mode;    // 位掩码，文件类型和文件权限
+	    nlink_t    st_nlink;    // 硬链接数
+	    uid_t    st_uid;    // 文件属主，用户 ID
+	    gid_t    st_gid;    // 文件属组，组 ID
+	    dev_t    st_rdev;    // 如果针对设备 i 节点，则此字段包含主、辅 ID
+	    off_t    st_size;    // 常规文件，则是文件字节数；符号链接，则是链接所指路径名的长度，字节为单位；对于共享内存对象，则是对象大小
+	    blksize_t    st_blsize;    // 分配给文件的总块数，块大小为 512 字节
+	    blkcnt_t    st_blocks;    // 实际分配给文件的磁盘块数量
+	    time_t    st_atime;        // 对文件上次访问时间
+	    time_t    st_mtime;        // 对文件上次修改时间
+	    time_t    st_ctime;        // 文件状态发生改变的上次时间
+	}
+	 */
+	Sys() interface{}   // underlying data source (can return nil)，底层数据来源（可以返回nil）
 }
 
 type FileMode uint32
@@ -90,24 +107,25 @@ type FileMode uint32
 const (
 	// The single letters are the abbreviations
 	// used by the String method's formatting.
-	ModeDir        FileMode = 1 << (32 - 1 - iota) // d: is a directory
-	ModeAppend                                     // a: append-only
-	ModeExclusive                                  // l: exclusive use
-	ModeTemporary                                  // T: temporary file; Plan 9 only
-	ModeSymlink                                    // L: symbolic link
-	ModeDevice                                     // D: device file
-	ModeNamedPipe                                  // p: named pipe (FIFO)
-	ModeSocket                                     // S: Unix domain socket
-	ModeSetuid                                     // u: setuid
-	ModeSetgid                                     // g: setgid
-	ModeCharDevice                                 // c: Unix character device, when ModeDevice is set
-	ModeSticky                                     // t: sticky
+	ModeDir        FileMode = 1 << (32 - 1 - iota) // d: is a directory，目录
+	ModeAppend                                     // a: append-only，只能写入，且只能写入末尾
+	ModeExclusive                                  // l: exclusive use，用于执行
+	ModeTemporary                                  // T: temporary file; Plan 9 only，临时文件；仅在Plan 9
+	ModeSymlink                                    // L: symbolic link，符号链接（不是快捷方式文件）
+	ModeDevice                                     // D: device file，设备
+	ModeNamedPipe                                  // p: named pipe (FIFO)，命名管道（FIFO）
+	ModeSocket                                     // S: Unix domain socket，Unix域socket
+	ModeSetuid                                     // u: setuid，设置uid
+	ModeSetgid                                     // g: setgid，设置gid
+	ModeCharDevice                                 // c: Unix character device, when ModeDevice is set，Unix字符设备，当ModeDevice设置为Unix时
+	ModeSticky                                     // t: sticky，只有root/创建者能删除/移动文件
 	ModeIrregular                                  // ?: non-regular file; nothing else is known about this file
 
 	// Mask for the type bits. For regular files, none will be set.
+	// 覆盖所有类型位，对于普通文件，所有这些位都不应该被设置
 	ModeType = ModeDir | ModeSymlink | ModeNamedPipe | ModeSocket | ModeDevice | ModeCharDevice | ModeIrregular
 
-	ModePerm FileMode = 0777 // Unix permission bits
+	ModePerm FileMode = 0777 // Unix permission bits，覆盖所有Unix权限位
 )
 
 func (m FileMode) String() string {
